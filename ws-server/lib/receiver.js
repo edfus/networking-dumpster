81c0f98421c52e3b2717af0bac080fe70b172f81
edfus <edfusedfus@gmail.com>
2021-03-23 11:03:48 +0800

    feat(ws): handle frame fragmentation / control

--- ws-server/lib/receiver.js
+++ ws-server/lib/receiver.js
@@ -14,21 +14,28 @@ import { Transform } from "stream";
   + - - - - - - - - - - - - - - - +-------------------------------+
  * 
  */
+const kSource = Symbol("source");
+
 class Parser extends Transform {
   constructor () {
     super({
       readableObjectMode: true
     });
+
+    this[kSource] = {
+      payloadData: Buffer.from(""),
+      type: "text"
+    };
   }
 
   _transform(chunk, encoding, cb) {
     const frame = {
       isFinal:       (chunk[0] & 128) === 128,   // 10000000
-      opCode:         chunk[0] & 0b1111,         // binary number
+      opcode:         chunk[0] & 0b1111,         // binary number
       isMasked:      (chunk[1] & 128) === 128,   // 10000000 
       payloadLength:  chunk[1] & 127,            // 01111111, the first part
-      // maskingKey:     '',
-      payloadData:    null
+      payloadData:    null,
+      type: "text"                               // text, binary, control
     };
 
     let startOffset = 2;
@@ -47,7 +54,13 @@ class Parser extends Transform {
         frame.payloadLength = payloadLength;
     }
 
-    const rawData = chunk.slice(startOffset, startOffset + frame.payloadLength);
+    const endOffset = startOffset + frame.payloadLength;
+    if(chunk.length < endOffset) {
+      return cb(new RangeError(`Expected chunk.length >= ${endOffset}`));
+    }
+
+    // get payload data
+    const rawData = chunk.slice(startOffset, endOffset);
     if (frame.isMasked) {
       const maskingKey = [
         chunk[startOffset++],
@@ -61,13 +74,70 @@ class Parser extends Transform {
           .map((byte, index) => byte ^ maskingKey[index % 4])
       );
     } else {
-      frame.payloadData = rawData
+      frame.payloadData = rawData;
     }
 
-    return cb(null, frame);
+    // frame fragmentation / control
+    // https://tools.ietf.org/html/rfc6455#section-5.2
+    switch (frame.opcode) {
+      case 0x0:
+        if(this[kSource].payloadData.length) { // first frame buffered
+          this[kSource].payloadData = Buffer.concat([
+            this[kSource].payloadData,
+            frame.payloadData
+          ]);
+
+          if(frame.isFinal) {
+            cb(null, this[kSource]);
+            this[kSource] = {
+              payloadData: Buffer.from(""),
+              type: "text"
+            };
+          } // terminated by a single frame with the FIN bit set and an opcode of 0
+        } else {
+          return cb(new Error("unexpected frame.opcode 0x0"));
+        }
+        return frame = null;
+      
+      case 0x1:
+        frame.type = "text";
+        if(frame.isFinal) {
+          return cb(null, frame);
+        } else {
+          this[kSource] = {
+            payloadData: frame.payloadData,
+            type: frame.type
+          };
+          return frame = null;
+        }
+
+      case 0x2:
+        frame.type = "binary";
+        if(frame.isFinal) {
+          return cb(null, frame);
+        } else {
+          this[kSource] = {
+            payloadData: frame.payloadData,
+            type: frame.type
+          };
+          return frame = null;
+        }
+      
+      case 0x8:
+        this[kSource] = null;
+        this.emit("opclose", frame);
+        return this.end(cb); // close the writable side
+      case 0x9:
+        return this.emit("ping", frame);
+      case 0xA:
+        return this.emit("pong", frame);
+      default:
+        return cb(new Error("an unknown opcode is received"));
+    }
   }
 
   _flush(cb) {
+    this[kSource] = null;
     return cb();
   }
 }
