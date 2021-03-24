f33a368d6ae53ca4cc4b3091c6b92d6649efbd5b
edfus <edfusedfus@gmail.com>
2021-03-24 22:07:14 +0800

    minor: delete certificate

--- miscellaneous/ssl/createRootCA.sh
+++ miscellaneous/ssl/createRootCA.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-mkdir ./out/
-winpty openssl genrsa -des3 -out ./out/rootCA.key 2048
-winpty openssl req -x509 -new -nodes -key ./out/rootCA.key -sha256 -days 1024 -out ./out/rootCA.pem
+winpty openssl genrsa -des3 -out ./out.hidden/rootCA.key 2048
+winpty openssl req -x509 -new -nodes -key ./out.hidden/rootCA.key -sha256 -days 1024 -out ./out.hidden/rootCA.pem
 read -p "Done."
--- miscellaneous/ssl/self-signCertificate.sh
+++ miscellaneous/ssl/self-signCertificate.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout cert.key -config ssl.conf
+openssl req -new -sha256 -nodes -out ./out.hidden/server.csr -newkey rsa:2048 -keyout ./out.hidden/server.key -config ssl.conf
 
-winpty openssl x509 -req -in server.csr -CA ./out/rootCA.pem -CAkey ./out/rootCA.key -CAcreateserial -out cert.pem -days 500 -sha256 -extfile v3.ext
+winpty openssl x509 -req -in ./out.hidden/server.csr -CA ./out.hidden/rootCA.pem -CAkey ./out.hidden/rootCA.key -CAcreateserial -out ./out.hidden/server.crt -days 500 -sha256 -extfile v3.ext
 read -p "Done."
