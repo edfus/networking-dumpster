2b76ca05b7891f07879a0eabc7469ad69bffab37
edfus <edfusedfus@gmail.com>
2021-03-31 15:27:30 +0800

    minor: update tls scripts

--- miscellaneous/ssl/createRootCA.sh
+++ miscellaneous/ssl/createRootCA.sh
@@ -1,4 +1,9 @@
-#! /bin/bash
+#!/bin/bash
+# export MSYS_NO_PATHCONV=1
 winpty openssl genrsa -des3 -out ./out.hidden/rootCA.key 2048
-winpty openssl req -x509 -new -nodes -key ./out.hidden/rootCA.key -sha256 -days 1024 -out ./out.hidden/rootCA.pem
+winpty openssl req -x509 -new -nodes \
+-key ./out.hidden/rootCA.key -sha256 \
+-subj "//X=1/C=US/ST=Here/L=LAN/O=Self Signed localhost CA" \
+-addext "extendedKeyUsage = serverAuth" \
+-days 1024 -out ./out.hidden/rootCA.crt 
 read -p "Done."
--- miscellaneous/ssl/self-signCertificate.sh
+++ miscellaneous/ssl/self-signCertificate.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 openssl req -new -sha256 -nodes -out ./out.hidden/server.csr -newkey rsa:2048 -keyout ./out.hidden/server.key -config ssl.conf
 
-winpty openssl x509 -req -in ./out.hidden/server.csr -CA ./out.hidden/rootCA.pem -CAkey ./out.hidden/rootCA.key -CAcreateserial -out ./out.hidden/server.crt -days 500 -sha256 -extfile v3.ext
+winpty openssl x509 -req -in ./out.hidden/server.csr -CA ./out.hidden/rootCA.crt -CAkey ./out.hidden/rootCA.key -CAcreateserial -out ./out.hidden/server.crt -days 500 -sha256 -extfile v3.ext
 read -p "Done."
